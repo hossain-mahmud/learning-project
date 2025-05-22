8429757d9bcbd8199c50e5cfaff46ccacd12d148
hossain-mahmud <hossainmahmud.cse@gmail.com>
2025-05-22 10:47:37 -0700

    Learn JS

--- learn JS/app.js
+++ learn JS/app.js
@@ -1,4 +1,41 @@
-var num1 =30;
-var num3 = 50;
+// var num1 =30;
+// var num3 = parseFloat("60.45645745");
 
-console.log(num1+num3);
+// console.log(num3 .toFixed(2));
+
+
+// var number1 = 40;
+// var number2 =50;
+
+// if (number1>number2){
+//     console.log("Yes Number one is Big")
+// }
+// else{
+//     console.log("Yes Number two is Big")
+// }
+
+
+var a = 20;
+var b = 40;
+var c = 76;
+ 
+
+// if(a<b && c<b){
+//     console.log("B is this Big");
+// }
+// else if(a<c){
+//     console.log("C is the Big");
+// }
+// else{
+//     console.log("A is the Big");
+// }
+
+var max=a;
+
+if(max<b && c<b){
+    max=b;
+}
+else{
+    max=c;
+}
+console.log("Max Value Is",max)
